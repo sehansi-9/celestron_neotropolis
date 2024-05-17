49a559ac0b10a13f72061bca45f339617a3071aa
supunTE <supunedirisuriya2002@gmail.com>
2024-05-17 13:34:32 +0530

    fix route issues

--- backend/app.ts
+++ backend/app.ts
@@ -1,6 +1,7 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import UserRoutes from './src/routes/UserRoutes';
+import express from "express";
+import bodyParser from "body-parser";
+import UserRoutes from "./src/routes/UserRoutes";
+import "dotenv/config";
 
 const app = express();
 
@@ -8,10 +9,10 @@ const app = express();
 app.use(bodyParser.json());
 
 // Routes
-app.use('/api/users', UserRoutes);
+app.use("/api/users", UserRoutes);
 
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
--- backend/src/actions/registerUser.ts
+++ backend/src/actions/registerUser.ts
@@ -2,7 +2,6 @@ import { addDoc, collection } from "firebase/firestore";
 import { User } from "../models";
 import { db } from "../firebase";
 import bcrypt from "bcrypt";
-import { nanoid } from "nanoid";
 
 export async function LoginUser({
   nic,
@@ -16,7 +15,7 @@ export async function LoginUser({
   role,
 }: Omit<User, "DTPCode">): Promise<User> {
   const encryptedPassword = await bcrypt.hash(password, 10);
-  const DTPCode = nanoid();
+  const DTPCode = "123";
   const user: User = {
     nic,
     firstName,
